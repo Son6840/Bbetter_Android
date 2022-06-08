1fcf7cc1df5b03f452f3b0a6af3843ad933a5e81
son <sangbae6840@gmail.com>
2022-06-08 23:37:04 +0900

    first

---
+++ app/src/main/java/com/son/bbetter/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.son.bbetter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
